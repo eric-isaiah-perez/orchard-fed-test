27a6d7b70d250a3e6a7f38dbef7fb4328fc2cfe4
Eric Isaiah Perez <ericisaiahperez@gmail.com>
2023-12-29 18:24:59 +0800

    UI for component 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './styles.css';
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     AOS.init();
 
-    jQuery('h3').addClass('yezzir');
+    // Display the title of the post when the user clicks on the anchor
+    jQuery('.post-anchor, .read-more').on('click', function () {
+        var nearestPostTitle = $(this).closest('.card-content').find('.post-title').text();
+        console.log('The element you clicked is ' + nearestPostTitle);
+    })
 });
